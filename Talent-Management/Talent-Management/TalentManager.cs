47afe0c7adddb9970819cbf2306ea2b88c570e5d
Roberts Gordins <roberts.gordins@bbsolutions.lv>
2023-10-09 19:39:32 +0300

    Added cost function.

--- Talent-Management/Talent-Management/TalentManager.cs
+++ Talent-Management/Talent-Management/TalentManager.cs
@@ -1,4 +1,7 @@
-﻿using Talent_Management.Utilities;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Talent_Management.Models;
+using Talent_Management.Utilities;
 
 namespace Talent_Management
 {
@@ -16,7 +19,25 @@ namespace Talent_Management
         {
             var movie = _movieSerializer.DeserializeMovie();
 
+            var price = CalculateSallary(movie.Actors, movie.Scenes);
+        }
 
+        private double CalculateSallary(List<Actor> actors, List<Scene> scenesSequence)
+        {
+            return actors.Sum(actor => CalculateSallary(actor, scenesSequence));
+        }
+
+        private double CalculateSallary(Actor actor, List<Scene> scenesSequence)
+        {
+            var firstScene = scenesSequence.First(scene => scene.ParticipantId.Contains(actor.Id));
+            var lastScene = scenesSequence.Last(scene => scene.ParticipantId.Contains(actor.Id));
+
+            var firstSceneId = scenesSequence.FindIndex(scene => scene.Id == firstScene.Id);
+            var lastSceneId = scenesSequence.FindIndex(scene => scene.Id == lastScene.Id);
+
+            var actingDays = lastSceneId - firstSceneId + 1;
+
+            return actingDays * actor.DaySalary;
         }
     }
 }
